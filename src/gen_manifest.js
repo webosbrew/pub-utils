2492bc84192a97b6b2c56561636ec8acdc20141d
Mariotaku <mariotaku.lee@gmail.com>
2022-01-04 22:02:51 +0900

    added rootRequired field

--- src/gen_manifest.js
+++ src/gen_manifest.js
@@ -16,10 +16,17 @@ parser.add_argument('-p', '--pkgfile', {required: true});
 parser.add_argument('-o', '--output', {required: true});
 parser.add_argument('-i', '--icon', {required: true});
 parser.add_argument('-l', '--link', {required: true});
+parser.add_argument('-r', '--root', {required: false});
 
 const args = parser.parse_args();
 
 const outfile = args.output;
+let rootRequired = false;
+if (args.root === 'optional') {
+    rootRequired = 'optional';
+} else if (args.root === 'true' || args.root === '1') {
+    rootRequired = true;
+}
 
 const appinfo = JSON.parse(fs.readFileSync(args.appinfo, {encoding: 'utf-8'}));
 
@@ -34,7 +41,7 @@ fs.writeFileSync(outfile, JSON.stringify({
     'appDescription': appinfo.appDescription,
     'iconUri': args.icon,
     'sourceUrl': args.link,
-    'rootRequired': false,
+    'rootRequired': rootRequired,
     'ipkUrl': pkgfile,
     'ipkHash': {
         'sha256': pkghash
